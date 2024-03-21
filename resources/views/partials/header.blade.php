f5d7815e5b1c050912b9154d021d6e58928236aa
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-21 20:38:37 +0100

    add and style header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,56 @@
 <header>
+
+  <div class=" bg-primary text-white">
+    <div class="container d-flex justify-content-end">
+      <div class=" px-4">
+        lorem
+      </div>
+
+      <div>
+        lorem
+      </div>
+    </div>
+  </div>
+
   <nav class="container">
 
-    <div>COMICS</div>
+    <div class=" py-2 d-flex justify-content-between align-items-center">
+        <div>
+            <a href="">
+                <img src="/images/dc-logo.png" alt="">
+            </a>
+        </div>
 
+        <div class=" d-flex ">
+            <div class=" px-3">
+                <a href="{{ route('home') }}">HOME</a>
+            </div>
 
-    <div>COMICS</div>
+            <div class="px-3">CHARACTERS</div>
 
-    <div>COMICS</div>
+            <div class="px-3">COMICS</div>
 
-    <div>COMICS</div>
+            <div class="px-3">TV</div>
 
-    <div>COMICS</div>
+            <div class="px-3">GAMES</div>
+
+            <div class="px-3">COLLECTIBLES</div>
+
+            <div class="px-3">VIDEOS</div>
+
+            <div class="px-3">FANS</div>
+
+            <div class="px-3">NEWS</div>
+
+            <div class="px-3">SHOP</div>
+        </div>
+
+        <div>
+            Search
+        </div>
+
+    </div>
 
   </nav>
-
-
-
-
-
-
+  
 </header>
