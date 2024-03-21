1defe0a0d516273726c893441a71665c73af90f4
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-21 18:54:05 +0100

    add comic thumbs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,24 @@
   <section class="jumbotron">
   </section>
 
-  
+  <section class="bg-dark">
+
+    <div class="row py-5 container m-auto">
+      @foreach ($comics as $comic)
+        <div class="col-2 py-3">
+          <div>
+            <img src="{{ $comic['thumb']}}" alt="" class="comic-thumb">
+          </div>
+
+          <div class="text-white text-center">
+            {{ $comic['title']}}
+          </div>
+        </div>
+      @endforeach
+    </div>
+    
+    <div class="text-center pb-3">
+      <button class="border border-primary text-white bg-primary px-5">LOAD MORE</button>
+    </div>
+  </section>
 @endsection
