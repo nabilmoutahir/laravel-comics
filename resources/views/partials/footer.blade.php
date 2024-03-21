5ccfa344a4dbc06f7522eaa32631b1c34e6e2cd2
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-21 20:04:21 +0100

    add and style footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,108 @@
-<footer class="bg-dark text-white">
-  <div class="container py-4">
-    &copy; {{ env('APP_AUTHOR', 'Tiziano Nicolai') }} {{ now()->year }}
-  </div>
+<footer>
+  <section class="contacts">
+
+    <div class="contacts-container container d-flex justify-content-between">
+      <div class="contacts-container-links d-flex flex-column flex-wrap align-content-start py-2">
+      <div>
+        <h4>DC</h4>
+        <div>Lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+      </div>
+      
+      <div>
+        <h4>DC</h4>
+        <div>lorem</div>
+        <div>lorem</div>
+      </div>
+
+      <div>
+        <div>lorem</div>
+        <div>lorem</div>
+      </div>
+
+      <div>
+        <h4>DC</h4>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+      </div>
+
+      <div>
+        <h4>DC</h4>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+        <div>lorem</div>
+      </div>
+    </div>
+
+    <div>
+      <img class="contacts-logo" src="/images/dc-logo-bg.png" alt="">
+    </div>
+    </div>
+
+    
+  </section>
+
+  <section class=" bg-dark">
+    <div class="container d-flex justify-content-between align-items-center py-5">
+
+      <div>
+        <button class="border border-primary bg-dark text-white p-2 ">SIGN UP NOW</button>
+      </div>
+
+      <div class=" d-flex justify-content-between align-items-center">
+        <div class="footer-right">FOLLOW US</div>
+
+        <div class="footer-right">
+          <a href="">
+            <img src="/images/footer-facebook.png" alt="">
+          </a>
+        </div>
+
+        <div class="footer-right">
+          <a href="">
+            <img src="/images/footer-periscope.png" alt="">
+          </a>
+        </div>
+
+        <div class="footer-right">
+          <a href="">
+            <img src="/images/footer-pinterest.png" alt="">
+          </a>
+        </div>
+
+        <div class="footer-right">
+          <a href="">
+            <img src="/images/footer-twitter.png" alt="">
+          </a>
+        </div>
+
+        <div class="footer-right">
+          <a href="">
+            <img src="/images/footer-youtube.png" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+
+
+
+
 </footer>
