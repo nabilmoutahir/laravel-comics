a9bd9e0599832e9427cbf7e3dd5cb01e773b55d0
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-21 22:09:41 +0100

    add view comics

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Comics')
+
+@section('main-content')
+
+<section class="bg-dark">
+
+    <div class="row py-5 container m-auto">
+      @foreach ($comics as $comic)
+        <div class="col-3 py-3">
+          <div>
+            <img src="{{ $comic['thumb']}}" alt="" class="comic-thumb">
+          </div>
+
+          <div class="text-white text-center">
+            {{ $comic['title']}}
+          </div>
+        </div>
+      @endforeach
+    </div>
+
+</section>
+
+
+@endsection
